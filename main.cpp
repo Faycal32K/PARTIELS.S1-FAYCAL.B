b2e9fe156e928e17e4da7619f181f16aff98e4f2
Faycal32K <faycalbouzidi4@gmail.com>
2024-02-08 11:19:00 +0000

    3.4

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 using namespace std;
 int main() {
- const float coutHoraire = 4.0;
+ 
+  const float coutHoraire = 4.0;
  int velosDispos = 20;
  int capacite= 20;
  int choix;
+do {
  cout << "****** Gestion d'une station Vélô ******" << endl << endl;
  cout << "-----------------------------------------" << endl;
  cout << "vélos disponible :" << velosDispos << endl ;
@@ -14,10 +16,23 @@ int main() {
  cout << "2. Restituer un vélô électrique" << endl;
  cout << "3. Estimer le coût de la location" << endl;
  cout << "4. Quitter l'application" << endl << endl;
- cin >> choix;
+ 
+   cin >> choix;
+  if (choix <= 0){
+    std::cout << "Choix invalide";
+  }
+  else if (choix >=5){
+    std::cout << "Choix invalide";
+  }
   switch (choix) {
       case 1:
-        std::cout << "Vous avez choisi Emprunter un vélô électrique";
+        cin << velosDispos;
+          cin << capacite;
+        std::cout << "vous avez prit un velo" <<
+       velosDispos - 1 <<":" << capacite + 1 << " 
+ nombre de place dispos"<<std::endl;
+        cin >> velosDispos;
+          cin >> capacite;
         break;
       case 2:
         std::cout << "Vous avez choisi Restituer un vélô électrique ";
@@ -28,7 +43,12 @@ int main() {
       case 4:
         std::cout << " au revoir";
         break;
-    
   }
- return 0;
-}
+  
+} while (choix != 4);
+    return 0;
+   }
+
+ 
+
+
