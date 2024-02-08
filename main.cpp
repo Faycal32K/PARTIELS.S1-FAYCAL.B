1b1ba636ec840cdc81a8feaf6958b4fc4794b12d
Faycal32K <faycalbouzidi4@gmail.com>
2024-02-08 09:59:56 +0000

    ajout de l'affichage des disponibilités

--- main.cpp
+++ main.cpp
@@ -4,10 +4,12 @@ int main() {
  const float coutHoraire = 4.0;
  int velosDispos = 20;
  int capacite= 20;
+ int placeDispos= 0 ;
  int choix;
  cout << "****** Gestion d'une station Vélô ******" << endl << endl;
  cout << "-----------------------------------------" << endl;
- //Partie à compléter 3.1
+ cout << "vélos disponible :" << velosDispos << endl ;
+ cout << "Points d'atache disponible :" << placeDispos << endl ;
  cout << "-----------------------------------------" << endl<< endl;
  cout << "1. Emprunter un vélô électrique" << endl;
  cout << "2. Restituer un vélô électrique" << endl;
